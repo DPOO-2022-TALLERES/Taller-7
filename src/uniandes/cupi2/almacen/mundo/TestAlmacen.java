5e95d0eac6009e91213216cb4c8f34b0cdf6cc1e
Valeria Caro Ramirez <v.caro@uniandes.edu.co>
2022-05-25 17:23:04 -0500

    Pruebas basicas, si quieren agregar mas me avizan

--- src/uniandes/cupi2/almacen/mundo/TestAlmacen.java
+++ src/uniandes/cupi2/almacen/mundo/TestAlmacen.java
@@ -11,14 +11,13 @@ import org.junit.jupiter.api.*;
 public class TestAlmacen {
 	
 	private Almacen almacen;
-	private Categoria categoria;
+	
 	
 	@BeforeEach
 	
 	public void setUp() throws AlmacenException {
 		this.almacen =  new Almacen(new File("data/datos.txt"));
-		
-		
+	
 	}
 	
 	
@@ -34,21 +33,45 @@ public class TestAlmacen {
 	
 	@Test
 	
-	public void agregarNodos() throws AlmacenException {
+	public void agregaryeliminarNodos() throws AlmacenException {
 		
 		this.almacen.agregarNodo("111", "Marca", "1322", "Puki");
 
 	    assertEquals("Puki", this.almacen.buscarNodo("1322").darNombre());
 	    
+	    this.almacen.eliminarNodo("1322");
+	    
+		assertNull(this.almacen.buscarNodo("1322"));
+		
+	    
+	}
+	
+	
+	@Test
+	
+	public void agregaryeliminarProducto() throws AlmacenException {
+		
+		this.almacen.agregarProducto("1111", "2213", "Pukini", "xd", 100);
+		
+		assertNotNull(this.almacen.darCategoriaRaiz().buscarProducto("2213"));
+		
+		this.almacen.eliminarProducto("2213");
+		
+		assertNull(this.almacen.darCategoriaRaiz().buscarProducto("2213"));
+		
+		
 	}
 	
 	@Test
 	
-	public void eliminarNodos() throws AlmacenException {
+	public void venderProducto() throws AlmacenException {
 		
-		 this.almacen.eliminarNodo("1322");
-		    
-		 assertNull(this.almacen.buscarNodo("1322"));
+		this.almacen.agregarProducto("1111", "2214", "ASUS ROG 14", "xd", 100);
+		
+		this.almacen.venderProducto("2214", 13);
+		
+		assertEquals(13,this.almacen.darCategoriaRaiz().buscarProducto("2214").darCantidadUnidadesVendidas());
+		
 		
 	}
 	
